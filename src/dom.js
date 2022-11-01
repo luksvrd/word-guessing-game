ded0f0043b180f4d047305c4acbffb98a649a5c9
Manav Misra <manavm@visionwebsoft.com>
2022-10-31 20:35:04 -0500

    Add all DOM queries

---
+++ src/dom.js
@@ -0,0 +1,6 @@
+export const lossesP = document.getElementById("losses");
+export const resetBtn = document.querySelector("main button");
+export const seconds = document.getElementById("seconds");
+export const startBtn = document.querySelector("header button");
+export const winsP = document.getElementById("wins");
+export const wordP = document.getElementById("word");
